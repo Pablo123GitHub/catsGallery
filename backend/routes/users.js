de15328b714f7db4b04b0f79a249db2e64f492ab
Pablo <paul.ladiv123@gmail.com>
2018-02-27 02:01:35 +0000

    React can use data served by express at /users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+
+function showUsers(){
+   return knex.select().from('users')
+       .then(function(row){
+           return row;
+       });
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{
-   	id: 1,
-   	username: "samsepi0l"
-   }, {
-   	id: 2,
-   	username: "D0loresH4ze"
-   }]);
+  showUsers().then((response)=>{
+          res.json(response);
+     });
 });
 
 module.exports = router;
